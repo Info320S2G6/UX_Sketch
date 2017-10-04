dd07f3fb472c1d99e3b8b0bf383ca37f8f689de6
lilkasian <lilkasian@users.noreply.github.com>
2017-10-05 12:14:29 +1300

    Taken Out Clock As code
    Clock is obselete

--- Assets/Scripts/PuzzleScripts/Safe.js
+++ Assets/Scripts/PuzzleScripts/Safe.js
@@ -1,4 +1,4 @@
-﻿var locks:TurningWheel[];
+/*﻿var locks:TurningWheel[];
 var clock:Clock;
 var correct:boolean=false;
 var safeWall:Transform;
@@ -23,6 +23,38 @@ function Check () {
 		secondTime=10*locks[4].currentNumber+locks[5].currentNumber;
 		
 		if(Mathf.Floor(clock.hours)==hourTime&&Mathf.Floor(clock.minutes)==minuteTime&&clock.seconds==secondTime){
+			correct=true;
+			GetComponent.<AudioSource>().Play();
+		}
+	}
+}
+
+function Update () {
+	if(correct){
+		safeWall.position.y=Mathf.MoveTowards(safeWall.position.y,heightInit+heightChange,moveSpeed*Time.deltaTime);
+	}
+}*/
+
+var locks:TurningWheel[];
+var correct:boolean=false;
+var safeWall:Transform;
+var heightChange:float=0.5;
+var moveSpeed:float=5;
+private var heightInit:float;
+var answer:float= 1;
+//Time stuff
+
+
+InvokeRepeating("Check",0.9,1);
+
+function Start(){
+	heightInit=safeWall.position.y;
+}
+function Check () {
+	if(!correct){
+		;
+
+		if(answer){
 			correct=true;
 			GetComponent.<AudioSource>().Play();
 		}
